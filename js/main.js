af04c1a1b7c296619fb0e801c40942cebd3983bc
lashaun owens <shaunbless@gmail.com>
2012-03-02 23:18:11 -0500

    fixed my github and js code

--- js/main.js
+++ js/main.js
@@ -1,37 +1,57 @@
-// Verify email alert
-// alert("Thank you for signing up!  Please check your email to activate your account.");
 //mobile DOM loader ($) mobile method
-var parseSignupForm = function (data) {
-	// uses form data here;
-	console.log(data); 
-};
-
 $('#info').live('pageinit', function(){
-	var rbform = $('#regform');
+	var rbform = $('#appform');
 	var	rberrorslink = $('#rberrorslink');
-	rbform.validate({
-		invalidHandler: function(form, validator){
-			rberrorslink.click();
-			var html = '';
-			for(var key in validator.submitted){
-				var label = $('label[for^="' + key +'"]').not('[generated]');
-				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
-				var fieldname = legend.length ? legend.text() : label.text();
-				html += '<li>' + fieldname + '</li>';
-			};
-			$("#signuperrors ul").html(html);
-		},
-		submitHandler: function(){
-			var data = rbform.serializeArray();
-			console.log(data);
-			storeData(data);
-			$.mobile.changePage($('#account'));
-		}
+	var formSave = $('#submit');
+	
+	// save form function
+	formSave.on('click', function(){
+		// form validation in jqm
+		rbform.validate({
+			invalidHandler: function(form, validator){
+				rberrorslink.click();
+				var html = '';
+				for(var key in validator.submitted){
+					var label = $('label[for^="' + key +'"]').not('[generated]');
+					var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+					var fieldname = legend.length ? legend.text() : label.text();
+					html += '<li>' + fieldname + '</li>';
+				}
+				$("#signuperrors ul").html(html);
+			},
+			submitHandler: function(){
+				var data = rbform.serializeArray();
+				storeData(data);
+				$.mobile.changePage($('#account'));
+			} 
+		}); 
+	}); 
+	var storeData = function (myData) {
+	    localStorage.setItem('signup_data', myData);
+	    alert("Your information has saved!");
+	};
+	
+var clearLink = $('#clear');
+	var data = localStorage.getItem('signup_data');
+	var editLink = $('#edit');
+	var clearData = function (myData) {
+		localStorage.clear('signup_data');
+	}; //ending clearData function
+	console.log("this page is working!");
+	// clearData function
+	clearLink.on('click', function(){
+
+		clearData(data);
+		alert("Data has been cleared");
+		$.mobile.changePage($('#account'));
+	
+	}); // ending clearLink function
+	
+	// change page to edit page
+	editLink.on('click', function (){
+		$.mobile.changePage($('#info'));
+		data;
 	});
 	
-	var storeData = function (myData) {
-		localStorage.setItem('signup_data', myData);
-		alert("Data has been saved!");
-	];
-
 });
+
